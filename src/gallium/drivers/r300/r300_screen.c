64e3feaa7b55ad94a4b94aa02ed6441d9a3d5aa9
Robin Kauffman <robink@creosotehill.org>
2014-05-04 15:58:59 -0700

    Possible fix for hopefully last regresion issue.

--- src/gallium/drivers/r300/r300_screen.c
+++ src/gallium/drivers/r300/r300_screen.c
@@ -174,7 +174,7 @@ static int r300_get_param(struct pipe_screen* pscreen, enum pipe_cap param)
         case PIPE_CAP_TEXTURE_QUERY_LOD:
         case PIPE_CAP_FAKE_SW_MSAA:
         case PIPE_CAP_SAMPLE_SHADING:
-        case PIPE_CAP_RASTERIZER_LOWER_LEFT_ORIGIN:
+/*      case PIPE_CAP_RASTERIZER_LOWER_LEFT_ORIGIN: // Not sure why this breaks things here */
             return 0;
 
         /* SWTCL-only features. */
